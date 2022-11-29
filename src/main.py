c793e46e968884e9e5f15d1ed3ba8050b7ad9f1e
matheusbat28 <matheusarrow28@gmail.com>
2022-11-29 19:25:41 -0300

    ajuste na função de vitoria

--- src/main.py
+++ src/main.py
@@ -7,25 +7,30 @@ from time import sleep
 def abrirPeguntas():
     with open('./src/pegunta.json', encoding='utf-8') as arquivo:
         return json.load(arquivo)
-    
+
+
 def quantidadePeguntas(lista):
     cont = 0
     for x in lista:
         cont += 1
     return cont
 
+
 def escolherPerguntas(lista, qtd):
     return lista[f'p{randint(1, qtd)}']
 
+
 def mostrarPegunta(perguntaEscolhida, numero, nome):
     print(f'Jogador {nome}')
-    print(str(numero) + '          -----Pergunta-----\n' + perguntaEscolhida['p'])
-    print('A) ' + perguntaEscolhida['a'] + '\n' + 
+    print(str(numero) + '          -----Pergunta-----\n' +
+          perguntaEscolhida['p'])
+    print('A) ' + perguntaEscolhida['a'] + '\n' +
           'B) ' + perguntaEscolhida['b'] + '\n' +
           'C) ' + perguntaEscolhida['c'] + '\n' +
           'D) ' + perguntaEscolhida['d'] + '\n' +
           'E) ' + perguntaEscolhida['e'] + '\n')
-    
+
+
 def verificarOpcao(perguntaEscolhida):
     while True:
         opcao = str(input('Qual sua resposta?\nR: ')).lower().strip()
@@ -39,25 +44,28 @@ def verificarOpcao(perguntaEscolhida):
         elif opcao == perguntaEscolhida['r']:
             print('Certa reposta!!!')
             return True
-        else: 
-            print('Errou!!\n' + 'Resposta correta: ' + perguntaEscolhida[perguntaEscolhida['r']])
+        else:
+            print('Errou!!\n' + 'Resposta correta: ' +
+                  perguntaEscolhida[perguntaEscolhida['r']])
             return False
-        
-        
+
+
 def mostrarJogadores(jogador):
     for chave, valor in jogador.items():
         print(f'{chave} {valor}')
-        
+
+
 def verificarVitoria(jogador1, jogador2):
     if jogador1['qtdAcertos'] < jogador2['qtdAcertos']:
         jogador2['status'] = 'Vitória'
-        jogador1['status'] = 'Derrota'   
+        jogador1['status'] = 'Derrota'
     elif jogador1['qtdAcertos'] == jogador2['qtdAcertos']:
         jogador1['status'] = 'Empate'
         jogador2['status'] = 'Empate'
     else:
         jogador1['status'] = 'Vitória'
         jogador2['status'] = 'Derrota'
+
 
 pgAntiga = []
 perguntas = abrirPeguntas()
@@ -85,8 +93,8 @@ if j1 == '':
     jogador1['nome'] = 'Jogador 1'
 else:
     jogador1['nome'] = j1
-    
-j2= str(input('Nome jogador n°2:\n')).strip().title()
+
+j2 = str(input('Nome jogador n°2:\n')).strip().title()
 if j2 == '':
     jogador2['nome'] = 'Jogador 2'
 else:
@@ -107,8 +115,7 @@ for x in range(1, partidas+1):
         else:
             pgAntiga.append(perguntaEscolhida)
             break
-            
-    
+
     if x % 2 == 0:
         mostrarPegunta(perguntaEscolhida, x, jogador1['nome'])
         if verificarOpcao(perguntaEscolhida):
@@ -125,9 +132,9 @@ for x in range(1, partidas+1):
             jogador2['qtdErro'] += 1
         sleep(2)
         os.system('cls') or None
-  
-verificarVitoria(jogador1, jogador2)  
-        
+
+verificarVitoria(jogador1, jogador2)
+
 mostrarJogadores(jogador1)
 print()
 mostrarJogadores(jogador2)
